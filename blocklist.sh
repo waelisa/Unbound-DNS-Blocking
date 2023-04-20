0255210c4e3b2fbd34552eeef76ac04e75cfe9d1
Wael Isa <24850589+waelisa@users.noreply.github.com>
2023-04-20 04:18:00 +0000

    add oisd list

--- blocklist.sh
+++ blocklist.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+#!/bin/bash
+#############################################################################################################################
+# The Apache-2.0 license 
+# Wael Isa
+# https://github.com/waelisa/Unbound-DNS-Blocking
+#############################################################################################################################
 cd /etc/unbound/unbound.conf.d/
-rm -fr hosts StevenBlack-hosts.conf
+rm -fr hosts StevenBlack-hosts.conf oisd-unbound.conf
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > StevenBlack-hosts.conf
-rm -fr hosts
+rm -fr hosts unbound
+wget https://big.oisd.nl/unbound
+mv unbound oisd-unbound.conf
 /sbin/service unbound reload
