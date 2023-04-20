9398a46d124ec1f5774acee92ac6603c28d038e0
Wael Isa <24850589+waelisa@users.noreply.github.com>
2023-04-20 06:33:49 +0000

    Update blocklist.sh

--- blocklist.sh
+++ blocklist.sh
@@ -6,10 +6,12 @@
 # https://github.com/waelisa/Unbound-DNS-Blocking
 #############################################################################################################################
 cd /etc/unbound/unbound.conf.d/
-rm -fr hosts StevenBlack-hosts.conf oisd-unbound.conf
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > StevenBlack-hosts.conf
-rm -fr hosts unbound
+#rm -fr hosts StevenBlack-hosts.conf
+#wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+#cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > StevenBlack-hosts.conf
+#rm -fr hosts unbound
+rm -fr oisd-unbound.conf
 wget https://big.oisd.nl/unbound
 mv unbound oisd-unbound.conf
+rm -fr unbound
 /sbin/service unbound reload
